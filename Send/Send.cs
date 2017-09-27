1ac8d0ffff7a8de5d069909168011c5c0601968d
Konstantin Milchev <konstantin.milchev@agepartnership.com>
2017-09-27 09:30:52 +0100

    Added a connection to the server

--- Send/Send.cs
+++ Send/Send.cs
@@ -4,9 +4,17 @@ namespace Send
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                }
+            }
         }
     }
 }
