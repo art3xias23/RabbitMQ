d31ca90db1b171be6a2c5a577ba0080a04cbc23c
Konstantin Milchev <konstantin.milchev@agepartnership.com>
2017-09-29 09:51:55 +0100

    Created the Routing RabbitMQ messaging service

---
+++ EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+
+class EmitLogDirect
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs",
+                                        type: "direct");
+
+                var severity = (args.Length > 0) ? args[0] : "info"; 
+
+                var message = (args.Length > 1) ? args.Join(" ").Skip(1).ToArray()) : "Hello World!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_logs",
+                                     routingKey: severity,
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] sent Severity {0} : Message {1}", message, severity);
+            }
+        }
+
+    }
+}
+
